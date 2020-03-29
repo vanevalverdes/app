0cab4b81451ca10111175bf2666e9fcd91cabbc3
Vanessa Valverdes <vane.valverdesantos@gmail.com>
2020-03-28 22:54:10 -0300

    Back-end de aplicacion

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,17 @@
+const connection = require('../database/connection')
+
+module.exports = {
+    async create(request, response) {
+        const {ong_id} = request.body;
+        
+        const ong = await connection('ongs')
+        .where('id', ong_id)
+        .select('name')
+        .first();
+
+        if (!ong){
+            return response.status(400).json({error: 'Not ONG found with this ID'});
+        }
+        return response.json(ong);
+    }
+};
